6689df1839748d5179ccaacdf74f40a70b9fb0f5
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-08 17:17:41 +0530

    Add files via upload

---
+++ EngineeringCollege.java
@@ -0,0 +1,10 @@
+class EngineeringCollege{
+      
+      public static void main(String []args){
+            int rollNumber = 100;
+            final double POINTER_CGPA = 9.14;
+            String studentName = "Tom Jerry";
+            System.out.println("Student Name : "+studentName+" Roll Number : "+rollNumber+" Percentage : "+POINTER_CGPA);
+       
+ }
+}
