f849ba5eb1e2d498b89df16842b8920e0bc8cb0f
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-08 15:52:03 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+/*18. Write a program in Java to find the area of any triangle.*/
+
+class Triangle{
+      public static void main(String []args){
+        double b=5 , h=5 , pi=3.14 , a ;
+        System.out.println("Base of triangle : "+b);
+        System.out.println("Height of triangle : "+h);
+        a = 0.5 * b * h;
+        System.out.println("Area of Triangle is : "+a);
+   }
+}
