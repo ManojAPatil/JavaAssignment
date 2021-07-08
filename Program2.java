32af65579d39c0c60e6c44d700b9758a4bfe0b75
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-08 15:46:31 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,12 @@
+/*2. Write a program in C++ to print the sum of two numbers.
+Sample Output:
+Print the sum of two numbers :
+-----------------------------------
+The sum of 29 and 30 is : 59*/
+
+class Program2{
+      public static void main(String []args){
+           int a=29;int b=30;
+           System.out.println("Sum of two numbers : "+(a+b));
+   }
+}
