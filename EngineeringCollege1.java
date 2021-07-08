ba56f28f8e867fc56e1c3bb949b82f4cc57c3f85
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-08 17:18:03 +0530

    Add files via upload

---
+++ EngineeringCollege1.java
@@ -0,0 +1,13 @@
+class EngineeringCollege1{
+      void studentDetails(){
+           int rollNumber = 100;
+            final double POINTER_CGPA = 9.14;
+            String studentName = "Tom Jerry";
+            System.out.println("Student Name : "+studentName+" Roll Number : "+rollNumber+" Percentage : "+POINTER_CGPA);
+
+   }
+      public static void main(String []args){
+           EngineeringCollege1 ec = new EngineeringCollege1();       
+           ec.studentDetails();       
+ }
+}
