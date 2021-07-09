136d8920aba2a4af7544ed49b029641c890af8fb
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:17:04 +0530

    Add files via upload

---
+++ Program29.java
@@ -0,0 +1,14 @@
+//29. Write a program in Java to compute quotient and remainder.
+
+class Program29{
+      public static void main(String []args){
+         int dividend=25 , divisor=3 , quotient ,remainder;
+         System.out.println("The Dividend is : "+dividend);
+         System.out.println("The Dividend is : "+divisor);
+         quotient = dividend / divisor ;
+         remainder = dividend % divisor ;
+         System.out.println("The Quotient of Division is : "+quotient);
+         System.out.println("The  Remainder of Division is : "+remainder); 
+ 
+  }
+}
