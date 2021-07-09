b44a57bb5676a4a2be935079828f45a66dc606ec
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:17:31 +0530

    Add files via upload

---
+++ Program30.java
@@ -0,0 +1,11 @@
+//30. Write a program in Java to compute the total and average of four numbers.
+
+class Program30{
+      public static void main(String []args){
+            int a=10 , b=20 , c=30 , d=40 ;
+            System.out.println("First two numbers is : "+a+" "+b);
+            System.out.println("Last two numbers is : "+c+" "+d);
+            System.out.println("The total of 4 numbers is : "+(a+b+c+d));
+            System.out.println("The average of 4 numbers is : "+((a+b+c+d)/4));
+   }
+}
