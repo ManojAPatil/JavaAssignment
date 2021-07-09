09177da74a573f7122a435b15eff6c308d709dc1
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:53:22 +0530

    Add files via upload

---
+++ Program35.java
@@ -0,0 +1,12 @@
+/*35. Write a program in Java to compute the specified expressions and print the output.
+Compute the specified expressions and print the output:
+------------------------------------------------------------
+Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : 2.13889
+*/
+
+class Program35{
+    public static void main(String []args){
+         double result = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+         System.out.println("Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : "+result);
+  }
+}
