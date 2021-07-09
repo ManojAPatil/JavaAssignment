8fc7ec2027df70972a7803fe69c97340841df764
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:15:29 +0530

    Add files via upload

---
+++ Temperature5.java
@@ -0,0 +1,10 @@
+//27. Write a program in C++ to convert temperature in Celsius to Kelvin.
+
+class Temperature5{
+       public static void main(String []args){
+            double c=26.85 ,k;
+            System.out.println("The Celcius value : "+c);
+            k = c + 273.15 ;
+            System.out.println("The Kelvin value : "+k);
+   }
+}
