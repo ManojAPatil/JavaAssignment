4457875b57c5501deff09cf9a4ee853b4be379dd
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:17:58 +0530

    Add files via upload

---
+++ Program31.java
@@ -0,0 +1,12 @@
+//31. Write a program in Java to input a single digit number and print a rectangular form of 4 columns and 6 rows.
+
+class Program31{
+      public static void main(String []args){
+          System.out.println("5 5 5 5");
+          System.out.println("5     5");
+          System.out.println("5     5");
+          System.out.println("5     5");
+          System.out.println("5     5");
+          System.out.println("5 5 5 5");
+  }
+}
