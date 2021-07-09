82b320b9ac628fd330b19c8c3b61be38a2ac6daa
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-09 12:16:34 +0530

    Add files via upload

---
+++ KiloToMiles.java
@@ -0,0 +1,10 @@
+//23. Write a program in Java that converts kilometers per hour to miles per hour.
+
+class KiloToMiles{
+      public static void main(String []args){
+            double km=100 , mi ;
+            System.out.println("The Kilometer is : "+km);
+            mi = km * 0.62137 ;
+            System.out.println("The Miles is : "+mi);
+  }
+}
