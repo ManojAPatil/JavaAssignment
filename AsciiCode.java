ec49e32f3a0c7c3f68dc4ecb214bd1f859b026b4
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-13 18:26:55 +0530

    Add files via upload

---
+++ AsciiCode.java
@@ -0,0 +1,10 @@
+//49. Write a program in C++ to print the code (ASCII code / Unicode code etc.) of a given character.
+
+//import java.util.*;
+class AsciiCode{
+      public static void main(String []args){
+         char c = 'a';
+         int ascii = c;
+         System.out.println("ASCII Value of "+c+" is : "+ascii);
+   }
+}
