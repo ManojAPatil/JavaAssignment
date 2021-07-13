8867a319d7534fcfffa6af0eb9e509369a8fd9f7
ManojAPatil <86048919+ManojAPatil@users.noreply.github.com>
2021-07-13 18:27:43 +0530

    Add files via upload

---
+++ Expresion.java
@@ -0,0 +1,8 @@
+class Expresion{
+      public static void main(String []args){
+             //int x = 3;
+             double x = 3;
+             double a = x / 2 + 3.5;
+             System.out.println(a);
+  }
+} 
